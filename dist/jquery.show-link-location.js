154adadf36c57bce8c6ca01e9694b04682e8c6ec
Yuusaku Miyazaki <toumin.m7@gmail.com>
2017-04-17 16:23:43 +0900

    Modified JSDocに対応。

--- dist/jquery.show-link-location.js
+++ dist/jquery.show-link-location.js
@@ -1,95 +1,114 @@
 /**
- * 単体テストを試すための、ごく簡単なjQueryプラグイン
+ * jQueryプラグインの例
+ * a要素のhref属性をテキストノードに追記する
+ * 参考: https://learn.jquery.com/plugins/basic-plugin-creation/#putting-it-together
+ * @file jQuery Plugin: jquery.show-link-location
+ * @version 1.0.0
+ * @author Yuusaku Miyazaki <toumin.m7@gmail.com>
+ * @license MIT License
  */
 (function($) {
 
 /**
  * jQueryにプラグインとして追加されるメソッド
- * @param {Object|string} options - オプションのオブジェクト、またはサブメソッド名
+ * @global
+ * @memberof jQuery
+ * @param {object|string} arg1 - オプション、またはサブメソッド名
+ * @param {string} [arg1.space=' '] - 空白
+ * @param {string} [arg1.bracketLeft='('] - 左かっこ
+ * @param {string} [arg1.bracketRight=')'] - 右かっこ
+ * @param {string} [arg1.cssClass='link-location'] - 挿入するspan要素のCSSクラス名
+ * @return {object} jQueryオブジェクト
  */
-$.fn.showLinkLocation = function(options) {
-  // サブメソッド実行に備えて、不定数の引数をまとめて配列にする
+$.fn.showLinkLocation = function(arg1) {
+  // サブメソッド実行に備えて、数の分からない引数をまとめて配列にする。
+  // 第1引数はサブメソッド名なので、それだけは除外する。
   var args = Array.prototype.slice.call(arguments, 1);
 
-  // 要素ごとにjQueryオブジェクトを返す
+  // メソッドチェーン持続のためにjQueryオブジェクトを返す
   return this.each(function() {
-    // すでにプラグインが適用されている場合は要素全体が格納される
+    // すでにプラグインを適用済みなら、保存したデータを呼び出す
     var objData = $(this).data('show-link-location');
 
-    if (objData && options in objData) {
+    // if (objData && arg1 in objData && arg1.charAt(0) != '_') {
+    if (objData && arg1 in objData) {
       // サブメソッドを実行する
-      objData[options].apply(objData, args);
-    } else {
+      objData[arg1].apply(objData, args);
+    } else if (typeof arg1 === 'object' || !arg1) {
       // プラグインを適用する (初回)
-      $(this).data('show-link-location', new ShowLinkLocation(this, options));
+      $(this).data('show-link-location', new ShowLinkLocation(this, arg1));
     }
+    // } else {
+    //   $.error('Method ' +  arg1 + ' does not exist on jQuery.showLinkLocation');
+    // }
   });
 };
 
 /**
- * プラグインの本体。クラス形式
+ * プラグインの本体
+ * @global
  * @class
+ * @param {object} elem - プラグインを適用する要素
+ * @param {object} option - ユーザから渡されたオプション
+ * @property {object} elem - プラグインを適用する要素
+ * @property {object} option - プラグインのオプション
  */
-function ShowLinkLocation(elem, options) {
+function ShowLinkLocation(elem, option) {
   this.elem = elem;
-  this.init(options);
+  this.option = this._initOption(option);
+  $(this.elem).append(this._createSpan());
 }
 
-// 以下は全て外部アクセス可能となる
-$.extend(ShowLinkLocation.prototype, {
+// privateメソッドは名前の先頭に"_"を付けている。
+$.extend(ShowLinkLocation.prototype, /** @lends ShowLinkLocation.prototype */ {
   /**
-   * 初期化する
+   * オプションを初期化する
    * @private
-   * @param Object options - ユーザから渡されたオプション
+   * @param {object} option - ユーザから渡されたオプション
+   * @return {object} 初期化済みのオプション
    */
-  init: function(options) {
-    this.setOptions(options);
-    this.insertText();
-  },
-  /**
-   * オプションを設定する
-   * @private
-   * @param Object options - ユーザから渡されたオプション
-   */
-  setOptions: function(options) {
-    this.options = $.extend({
+  _initOption: function(option) {
+    return $.extend({
       space: ' ',
-      braceLeft: '(',
-      braceRight: ')',
+      bracketLeft: '(',
+      bracketRight: ')',
       cssClass: 'link-location'
-    }, options);
+    }, option);
   },
   /**
-   * <span>を追加する
+   * 挿入用のspan要素を生成する
    * @private
+   * @return {object} span要素
    */
-  insertText: function() {
-    var linkLocation = $(this.elem).attr('href');
-    var span = $('<span>')
+  _createSpan: function() {
+    return $('<span>')
       .text(
-        this.options.space +
-        this.options.braceLeft +
-        linkLocation +
-        this.options.braceRight
+        this.option.space +
+        this.option.bracketLeft +
+        $(this.elem).attr('href') +
+        this.option.bracketRight
       )
-      .addClass(this.options.cssClass);
-    $(this.elem).append(span);
+      .addClass(this.option.cssClass);
   },
   /**
-   * <span>を削除する
+   * プラグインの適用を解除する
    * @public
    */
   destroy: function() {
-    $(this.elem).find('.' + this.options.cssClass).remove();
+    // プラグインによって追加した要素を削除
+    $(this.elem).find('.' + this.option.cssClass).remove();
+
+    // dataを削除
+    $(this.elem).removeData('show-link-location');
   },
   /**
-   * <span>を作り直す
+   * プラグインを再適用する
    * @public
-   * @param Object options - ユーザから渡されたオプション
+   * @param {object} option - ユーザから渡されたオプション
    */
-  reset: function(options) {
+  reset: function(option) {
     this.destroy();
-    this.init(options);
+    $(this.elem).showLinkLocation(option);
   }
 });
 
